9258547351577b9a2d9633251a45e3b9fdb31ef9
emilsangil <sangilemiljan@gmail.com>
2022-06-01 05:06:03 -0700

    added view recipe

---
+++ js/testview.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    loadTestView();
+}
+
+function loadTestView(){
+    console.log('reload')
+    recipe = JSON.parse(localStorage.getItem('recipe'));
+
+    recipe.forEach(rec => {
+        document.getElementById('recipename').value = rec.recipename;
+        document.getElementById('recipedescription').value = rec.recipedescription;
+        document.getElementById('ingredients').value = rec.ingredients;
+        document.getElementById('direction').value = rec.direction;
+    })
+}
